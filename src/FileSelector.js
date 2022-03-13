59be28a3c516423feee10be3731b938d2c67d159
PMDimitrova <palmina.dimitrova@gmail.com>
2022-03-13 22:00:24 +0200

    logic for reading from csv file

--- src/FileSelector.js
+++ src/FileSelector.js
@@ -1,16 +1,65 @@
-import {Button, Form} from "react-bootstrap";
 import './FileSelector.css';
+import {useState} from "react";
 
 export default function FileSelector() {
-    return (
-        <>
-            <Form className="formWrapper">
-                <Form.Group controlId="formFile" className="mb-3">
-                    <Form.Label className="caption">Please select file with data for the employees working days:</Form.Label>
-                    <Form.Control type="file" />
-                </Form.Group>
-                <Button className="proceedBtn" variant="light">Proceed</Button>
-            </Form>
-        </>
-    )
+
+    const [csvFile, setCsvFile] = useState(null);
+    const [csvArray, setCsvArray] = useState([]);
+
+    const processCSV = (str, delim=', ') => {
+        const headers = str.slice(0,str.indexOf('\n')).split(delim);
+        const rows = str.slice(str.indexOf('\n')+1).split('\n');
+
+        const newArray = rows.map( row => {
+            const values = row.split(delim);
+            return headers.reduce((obj, header, i) => {
+                obj[header] = values[i];
+                return obj;
+            }, {});
+        })
+
+        setCsvArray(newArray);
+        showResult()
+
+    }
+
+    const showResult = () => {
+        console.log(csvArray)
+    }
+
+    const submit = () => {
+        const file = csvFile;
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const text = e.target.result;
+            console.log(text);
+            processCSV(text);
+        }
+
+        reader.readAsText(file);
+    }
+
+    return(
+        <form id='csv-form'>
+            <input
+                type='file'
+                accept='.csv'
+                id='csvFile'
+                onChange={(e) => {
+                    setCsvFile(e.target.files[0])
+                }}
+            >
+            </input>
+            <br/>
+            <button onClick={(e) => {
+                e.preventDefault()
+                if(csvFile)
+                    {submit()}
+            }}>
+                Show longest time
+            </button>
+        </form>
+    );
+
 }
